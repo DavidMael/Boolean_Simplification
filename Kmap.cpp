35c94e14b33cc2c0e23cff7e3c18dd04ad0a82ef
David <dc2619@ic.ac.uk>
2020-05-08 14:59:28 +0200

    horizontaldoubs debugged

--- Kmap.cpp
+++ Kmap.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 int main()
 {
+    kmap testmap {1, 1, 0, 0, 1, 1, 1, 1};
 
+    for(int i =  0; i<testmap.squares.size(); i++)
+    {
+        for(int j = 0; j<testmap.squares[1].size(); j++)
+        {
+            cout << i << ";" << j << " " << testmap.squares[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    vector<doub> onedoubs = testmap.horizontaldoubs();
+
+    for(int i = 0; i<onedoubs.size(); i++)
+    {
+        cout << onedoubs[i].sone.first << ";" << onedoubs[i].sone.second << " " << onedoubs[i].stwo.first << ";" << onedoubs[i].stwo.second << endl;
+    }    
 }
--- Kmap.hpp
+++ Kmap.hpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 struct doub
-//basically a pair of pairs of pairs, renamed for brevity in declarations
+//basically a pair of pairs of ints, renamed for brevity in declarations and consistency  with quad and oct class
 {
     //pairs the i (height) and j(width) coordinates of the two squares in the pair
-    pair<int, int> first;
-    pair<int, int> second;
-}
+        pair<int, int> sone;
+        pair<int, int> stwo;
+};
 
 class kmap
 {
-    private:
-        vector<vector<bool> bits;
+    //private: later make a reader function and make data member private
+        
     public:
+        vector<vector<bool>> squares;
+
         kmap(bool a,bool b,bool c,bool d, bool e, bool f, bool g, bool h)
         {
-            bits {{a, b, c, d}, {e, f, g, h}};
+            squares = {{a, b, c, d}, {e, f, g, h}};
         }
 
         //scans the kmap for horizontal doubles
-        vector<doub> horizontaldoubs(const kmap& k)
+        vector<doub> horizontaldoubs()
         {
-            //holder for detected doubles
-            doub hold;
             //initialise vector of doubles
-            vect vector<doub>;
+            vector<doub>  vect;
             //height of the kmap
-            int height = k.bits.size();
+            int height = squares.size();
             //width of the kmap
-            int width = k.bits[1].size();
+            int width = squares[1].size();
             //cycle through each square of the kmap 
             for(int i = 0; i<height; i++)
             {
@@ -39,17 +39,15 @@ class kmap
                     //square not at the end of the line
                     if(j != (width-1))
                     {
-                        if(k[i][j] == 1 && k[i][j+1] == 1)
+                        if(squares[i][j] == 1 && squares[i][j+1] == 1)
                         {
-                            hold = {pair<i, j>, pair<i, (j+1)>};
-                            vect.push_back(hold);
+                            vect.push_back({ {i, j}, {i, (j+1)} });
                         } 
                     } else {
                     //square at end of line    
-                        if(k[i][j] == 1 && k[0][0] == 1)
+                        if(squares[i][j] == 1 && squares[0][0] == 1)
                         {
-                            hold = {pair<i, j>, pair<i, (j+1)>};
-                            vect.push_back(hold);
+                            vect.push_back({ {i, j}, {0, 0} });
                         }  
                     }
                 }
